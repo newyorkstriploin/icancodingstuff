fda7af79f9fded64d7f2b1f351edfe36267411fa
newyorkstriploin <160943093+newyorkstriploin@users.noreply.github.com>
2024-03-21 14:08:25 -0400

    Add files via upload

---
+++ python exponent calculator.py
@@ -0,0 +1,85 @@
+#Step 1
+#num = int(input('this number will be the base number'))
+#exp = int(input('this number will be the exponent'))
+
+
+
+#for i in range(num):
+#    temp = (num ** exp)
+#    print(temp)
+
+
+#Step 2
+#rows = int(5)  
+
+
+#for num in range(rows+1):  
+  
+#        for i in range(num):  
+#            print(num, end=" ")        
+#        print()
+
+
+#Step 3
+#def num_line():
+#    num1 = int(input("what is your first number"))
+#    num2 = int(input("what is your second number"))
+#    times1 = num2 - num1 + 1
+#    times2 = num1 - num2 + 1
+#    if num1 < num2:
+#        for i in range(times1):
+#            print(num1)
+#            num1 = num1 +1
+#    else:
+#        for i in range(times2):
+#            print(num2)
+#            num2 = num2 +1
+#num_line()
+
+
+#Step 4 
+#def it_is_a_prime_number(a):
+#    if a <= 1:
+#        return False
+#    for i in range(2, int(a ** 0.5)+1):
+#        if a % i == 0:
+#            return False
+#
+#    return True
+# 
+#def s():
+#    a = int(input("Enter a numer: "))
+#    b = int(input("Enter another number: "))
+   
+#    if a > b:
+#        r = range(b, a)
+#    elif b > a:
+#        r = range(a, b)
+ 
+#    for w in r:
+#        if it_is_a_prime_number(w):
+#            print(w)
+ 
+#s()
+#print("")
+
+
+#step 5
+#def factorial(number):
+#    a = 1
+#    b = range(1, (number+1))
+ 
+#    for i in b:
+#        a = a * i
+   
+#    print(a)
+
+#number = int(input("Enter a number: "))
+#factorial(number)
+
+
+
+
+
+
+
